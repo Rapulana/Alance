fddfaa3e8cde5b11d3119739eb3302ac92f3020c
Pascal Rapulana <rapulanapascal15@gmail.com>
2025-11-15 09:15:20 +0200

    Add sidebars.js for documentation navigation

---
+++ website/sidebars.js
@@ -0,0 +1,13 @@
+module.exports = {
+  docs: [
+    'intro',
+    'getting-started',
+    'quantum-overview',
+    'alance-sdk',
+    'examples',
+    'api-reference',
+    'contributing',
+    'roadmap',
+    'release-notes',
+  ],
+};
